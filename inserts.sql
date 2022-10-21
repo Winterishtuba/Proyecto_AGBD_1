f24001db95e78b392423203c4cca19e112b8baaa
Xtres5 <Federicoz2005@gmail.com>
2022-10-21 09:57:47 -0300

    rezemos

--- inserts.sql
+++ inserts.sql
@@ -23,62 +23,60 @@ INSERT INTO deposito VALUES ('D-ROS-01', 'ROS-01', 'Derqui 5957', '+54 341 7612-
 INSERT INTO persona VALUES ('', 'Carlos', 'Menem', '90', '30241245', '1164893822','Balcarce 50', 'CarlosSaulMenemFerrari@gmail.com');
 INSERT INTO persona VALUES ('', 'Mauricio', 'Macri','63' ,'38898345', '1164884522','Av. Maipú 2100', 'MacriMaurigato@gmail.com');
 
-INSERT INTO persona VALUES ('', 'Arturo', 'Mateu', '54', '34312678', '1137825490','Av. independecia 1507', 'Arturmateu@gmail.com');
-INSERT INTO persona VALUES ('', 'Jairo', 'Lloris','34' ,'32548129', '1112904378','Av. La Plata 7284', 'Jaillora@gmail.com');
-INSERT INTO persona VALUES ('', 'Hugo', 'Sobrino', '61', '24381923', '1138921298','Jose Bonifacio 4542', 'Hugosobri@gmail.com');
-INSERT INTO persona VALUES ('', 'Gustavo', 'Teruel','45' ,'21748983', '1187598729','Pumacahua 1457', 'Gustavoteruel@gmail.com');
-INSERT INTO persona VALUES ('', 'Feliciano', 'Amores', '34', '65238129', '1132789812','Jose Enrique Rodo 1839', 'Felicianoamor@gmail.com');
-INSERT INTO persona VALUES ('', 'Rocio', 'Aznar','25' ,'38981846', '1192182309','Av General Paz 1238', 'Rocioazna@gmail.com');
-INSERT INTO persona VALUES ('', 'Myriam', 'Murillo', '84', '28547812', '1109217823','Marcos Sastre 2415', 'Myriammuri@gmail.com');
-INSERT INTO persona VALUES ('', 'Jamal', 'Ubeda','72' ,'23764981', '1189217632','Av Juan Bautista Alberdi 2617', 'Jamalubeda@gmail.com');
-INSERT INTO persona VALUES ('', 'Alex', 'Gonzales', '81', '23918965', '1132891923','Fonrouge 2315', 'Alexgonza@gmail.com');
-INSERT INTO persona VALUES ('', 'Khadija', 'Camara','75' ,'23847194', '1102917823','Av Rivadavia 8219', 'Khadicama@gmail.com');
+INSERT INTO persona VALUES ('', 'Arturo', 'Mateu', '54', '34312678', '+54 11 3782-5490','Av. independecia 1507', 'Arturmateu@gmail.com');
+INSERT INTO persona VALUES ('', 'Jairo', 'Lloris','34' ,'32548129', '+54 11 1290-4378','Av. La Plata 7284', 'Jaillora@gmail.com');
+INSERT INTO persona VALUES ('', 'Hugo', 'Sobrino', '61', '24381923', '+54 11 3892-1298','Jose Bonifacio 4542', 'Hugosobri@gmail.com');
+INSERT INTO persona VALUES ('', 'Gustavo', 'Teruel','45' ,'21748983', '+54 11 8759-8729','Pumacahua 1457', 'Gustavoteruel@gmail.com');
+INSERT INTO persona VALUES ('', 'Feliciano', 'Amores', '34', '65238129', '+54 11 3278-9812','Jose Enrique Rodo 1839', 'Felicianoamor@gmail.com');
+INSERT INTO persona VALUES ('', 'Rocio', 'Aznar','25' ,'38981846', '+54 11 9218-2309','Av General Paz 1238', 'Rocioazna@gmail.com');
+INSERT INTO persona VALUES ('', 'Myriam', 'Murillo', '84', '28547812', '+54 11 0921-7823','Marcos Sastre 2415', 'Myriammuri@gmail.com');
+INSERT INTO persona VALUES ('', 'Jamal', 'Ubeda','72' ,'23764981', '+54 11 8921-7632','Av Juan Bautista Alberdi 2617', 'Jamalubeda@gmail.com');
+INSERT INTO persona VALUES ('', 'Alex', 'Gonzales', '81', '23918965', '+54 11 3289-1923','Fonrouge 2315', 'Alexgonza@gmail.com');
+INSERT INTO persona VALUES ('', 'Khadija', 'Camara','75' ,'23847194', '+54 11 0291-7823','Av Rivadavia 8219', 'Khadicama@gmail.com');
 
-INSERT INTO persona VALUES ('', 'Marcela', 'Brito', '41', '34981765', '1189212378','Pola 389', 'Marcelabri@gmail.com');
-INSERT INTO persona VALUES ('', 'Alexis', 'Cañadas','49' ,'31467891', '1192182398','Av Escalada 1245', 'Alexiscaña@gmail.com');
-INSERT INTO persona VALUES ('', 'Joan', 'Portela', '29', '23857910', '1132781278','Av Eva Peron 231', 'Joanpuerta@gmail.com');
-INSERT INTO persona VALUES ('', 'Arancha', 'Ahmed','19' ,'23847321', '1194382389','Av Juan B Justo 8271', 'Aranchaahme@gmail.com');
-INSERT INTO persona VALUES ('', 'Bianca', 'Marrero', '28', '39874018', '1132148943','Miralla 2512', 'Biancamerre@gmail.com');
-INSERT INTO persona VALUES ('', 'César', 'Carbonell','30' ,'28871674', '1192812389','Corvalan 8234', 'Cesarcarbon@gmail.com');
-INSERT INTO persona VALUES ('', 'Irati', 'Fidalgo', '24', '28174895', '1123091209','Araujo 4124', 'Iratifidel@gmail.com');
-INSERT INTO persona VALUES ('', 'Naroa', 'Talavera','62' ,'30982716', '1121982389','Camacua 4125', 'Naroatala@gmail.com');
-INSERT INTO persona VALUES ('', 'Alberto', 'Gago', '31', '39817298', '1191282398','Av Carabobo 8291', 'Albertogagoneta@gmail.com');
-INSERT INTO persona VALUES ('', 'Sabrina', 'Farre','42' ,'30928184', '1192813289','Av Rivadavia 4152', 'Sabriferrerorochet@gmail.com');
+INSERT INTO persona VALUES ('', 'Marcela', 'Brito', '41', '34981765', '+54 11 8921-2378','Pola 389', 'Marcelabri@gmail.com');
+INSERT INTO persona VALUES ('', 'Alexis', 'Cañadas','49' ,'31467891', '+54 11 9218-2398','Av Escalada 1245', 'Alexiscaña@gmail.com');
+INSERT INTO persona VALUES ('', 'Joan', 'Portela', '29', '23857910', '+54 11 3278-1278','Av Eva Peron 231', 'Joanpuerta@gmail.com');
+INSERT INTO persona VALUES ('', 'Arancha', 'Ahmed','19' ,'23847321', '+54 11 9438-2389','Av Juan B Justo 8271', 'Aranchaahme@gmail.com');
+INSERT INTO persona VALUES ('', 'Bianca', 'Marrero', '28', '39874018', '+54 11 3214-8943','Miralla 2512', 'Biancamerre@gmail.com');
+INSERT INTO persona VALUES ('', 'César', 'Carbonell','30' ,'28871674', '+54 11 9281-2389','Corvalan 8234', 'Cesarcarbon@gmail.com');
+INSERT INTO persona VALUES ('', 'Irati', 'Fidalgo', '24', '28174895', '+54 11 2309-1209','Araujo 4124', 'Iratifidel@gmail.com');
+INSERT INTO persona VALUES ('', 'Naroa', 'Talavera','62' ,'30982716', '+54 11 2198-2389','Camacua 4125', 'Naroatala@gmail.com');
+INSERT INTO persona VALUES ('', 'Alberto', 'Gago', '31', '39817298', '+54 11 9128-2398','Av Carabobo 8291', 'Albertogagoneta@gmail.com');
+INSERT INTO persona VALUES ('', 'Sabrina', 'Farre','42' ,'30928184', '+54 11 9281-3289','Av Rivadavia 4152', 'Sabriferrerorochet@gmail.com');
 
-INSERT INTO persona VALUES ('', 'Jacinta', 'Ares', '32', '30982761', '1121892389','Av Directorio 8092', 'Jacintares@gmail.com');
-INSERT INTO persona VALUES ('', 'Fidel', 'Ubeda','12' ,'30918273', '1190122309','Av Perito Moreno 5125', 'Fideluba@gmail.com');
-INSERT INTO persona VALUES ('', 'Francisco', 'Clemente', '69', '31874627', '1112093921','Av Lacarra 8729', 'Francisclemencia@gmail.com');
-INSERT INTO persona VALUES ('', 'Aitana', 'Puente','21' ,'32857284', '1112902390','Mozart 1902', 'Aitipuente@gmail.com');
-INSERT INTO persona VALUES ('', 'Anibal', 'Rial', '53', '40892426', '1110292390','White 1829', 'Anibalreal@gmail.com');
-INSERT INTO persona VALUES ('', 'Florencio', 'Montiel','62' ,'41827134', '1143891892','Bragado 2934', 'Florenciomontiel@gmail.com');
-INSERT INTO persona VALUES ('', 'Noe', 'Portillo', '19', '38172847', '1129182389','Montiel 5224', 'Noeporti@gmail.com');
-INSERT INTO persona VALUES ('', 'Mireia', 'Nogales','24' ,'37819203', '1102912390','Laguna 2783', 'Mireianogale@gmail.com');
-INSERT INTO persona VALUES ('', 'Myriam', 'Moreira', '65', '42857283', '1198212389','Zelada 8271', 'Myriamore@gmail.com');
-INSERT INTO persona VALUES ('', 'Ayman', 'Trigo','54' ,'39812738', '1112982389','Av Gaona 10582', 'Aymantrigo@gmail.com');
+INSERT INTO persona VALUES ('', 'Jacinta', 'Ares', '32', '30982761', '+54 11 2189-2389','Av Directorio 8092', 'Jacintares@gmail.com');
+INSERT INTO persona VALUES ('', 'Fidel', 'Ubeda','12' ,'30918273', '+54 11 9012-2309','Av Perito Moreno 5125', 'Fideluba@gmail.com');
+INSERT INTO persona VALUES ('', 'Francisco', 'Clemente', '69', '31874627', '+54 11 1209-3921','Av Lacarra 8729', 'Francisclemencia@gmail.com');
+INSERT INTO persona VALUES ('', 'Aitana', 'Puente','21' ,'32857284', '+54 11 1290-2390','Mozart 1902', 'Aitipuente@gmail.com');
+INSERT INTO persona VALUES ('', 'Anibal', 'Rial', '53', '40892426', '+54 11 1029-2390','White 1829', 'Anibalreal@gmail.com');
+INSERT INTO persona VALUES ('', 'Florencio', 'Montiel','62' ,'41827134', '+54 11 4389-1892','Bragado 2934', 'Florenciomontiel@gmail.com');
+INSERT INTO persona VALUES ('', 'Noe', 'Portillo', '19', '38172847', '+54 11 2918-2389','Montiel 5224', 'Noeporti@gmail.com');
+INSERT INTO persona VALUES ('', 'Mireia', 'Nogales','24' ,'37819203', '+54 11 0291-2390','Laguna 2783', 'Mireianogale@gmail.com');
+INSERT INTO persona VALUES ('', 'Myriam', 'Moreira', '65', '42857283', '+54 11 9821-2389','Zelada 8271', 'Myriamore@gmail.com');
+INSERT INTO persona VALUES ('', 'Ayman', 'Trigo','54' ,'39812738', '+54 11 1298-2389','Av Gaona 10582', 'Aymantrigo@gmail.com');
 
-INSERT INTO persona VALUES ('', 'Chloe', 'Olmo', '43', '41284912', '1121982398','Tandil 812', 'Chloeolmo@gmail.com');
-INSERT INTO persona VALUES ('', 'Martin', 'Gimenez','23' ,'32854915', '1123892198','Av Nazca 5215', 'Martingime@gmail.com');
-INSERT INTO persona VALUES ('', 'Jose', 'Gomez', '29', '37982485', '1112982398','Portela 8912', 'Josegome@gmail.com');
-INSERT INTO persona VALUES ('', 'Javier', 'Ozkosger','27' ,'31924098', '1193281298','Jose Marti 1892', 'Javiozko@gmail.com');
-INSERT INTO persona VALUES ('', 'Santiago', 'Rey', '72', '34182945', '1191282389','Pergamino 8241', 'Santirey@gmail.com');
-INSERT INTO persona VALUES ('', 'Lucas', 'Hadjilias','32' ,'34819284', '1181293289','Zuviria 2852', 'Lukihadji@gmail.com');
-INSERT INTO persona VALUES ('', 'Lucas', 'Elmassian', '43', '23819209', '1154982189','Bacacay 8291', 'Lucaselmascapo@gmail.com');
-INSERT INTO persona VALUES ('', 'Judith', 'Wu','18' ,'42812819', '1189122389','Yerbal 6097', 'judithwu@gmail.com');
-INSERT INTO persona VALUES ('', 'Josefina', 'Haro', '29', '39691820', '1128192389','Lautaro 8291', 'Josefinaharo@gmail.com');
-INSERT INTO persona VALUES ('', 'Javier', 'Quiles','54' ,'43019278', '1123782178','Thorne 512', 'Javiquiles@gmail.com');
+INSERT INTO persona VALUES ('', 'Chloe', 'Olmo', '43', '41284912', '+54 11 2198-2398','Tandil 812', 'Chloeolmo@gmail.com');
+INSERT INTO persona VALUES ('', 'Martin', 'Gimenez','23' ,'32854915', '+54 11 2389-2198','Av Nazca 5215', 'Martingime@gmail.com');
+INSERT INTO persona VALUES ('', 'Jose', 'Gomez', '29', '37982485', '+54 11 1298-2398','Portela 8912', 'Josegome@gmail.com');
+INSERT INTO persona VALUES ('', 'Javier', 'Ozkosger','27' ,'31924098', '+54 11 9328-1298','Jose Marti 1892', 'Javiozko@gmail.com');
+INSERT INTO persona VALUES ('', 'Santiago', 'Rey', '72', '34182945', '+54 11 9128-2389','Pergamino 8241', 'Santirey@gmail.com');
+INSERT INTO persona VALUES ('', 'Lucas', 'Hadjilias','32' ,'34819284', '+54 11 8129-3289','Zuviria 2852', 'Lukihadji@gmail.com');
+INSERT INTO persona VALUES ('', 'Lucas', 'Elmassian', '43', '23819209', '+54 11 5498-2189','Bacacay 8291', 'Lucaselmascapo@gmail.com');
+INSERT INTO persona VALUES ('', 'Judith', 'Wu','18' ,'42812819', '+54 11 8912-2389','Yerbal 6097', 'judithwu@gmail.com');
+INSERT INTO persona VALUES ('', 'Josefina', 'Haro', '29', '39691820', '+54 11 2819-2389','Lautaro 8291', 'Josefinaharo@gmail.com');
+INSERT INTO persona VALUES ('', 'Javier', 'Quiles','54' ,'43019278', '+54 11 2378-2178','Thorne 512', 'Javiquiles@gmail.com');
 
-INSERT INTO persona VALUES ('', 'Bilal', 'Novo', '65', '34871890', '1134981289','Av Asamblea 5125', 'Bilalnovo@gmail.com');
-INSERT INTO persona VALUES ('', 'Eleuterio', 'Aliaga','43' ,'23890197', '1187342187','Tejedor 252', 'Eleutorioaliaga@gmail.com');
-INSERT INTO persona VALUES ('', 'Younes', 'Vazquez', '87', '32892987', '1112346754','Avellaneda 8924', 'Younesvazquez@gmail.com');
-INSERT INTO persona VALUES ('', 'Ona', 'Hinojosa','37' ,'39872910', '1121892389','Av Pavon 8294', 'Onahinojosa@gmail.com');
-INSERT INTO persona VALUES ('', 'Yaiza', 'Calle', '74', '28901827', '1191282398','Av Jose Maria Moreno 5829', 'Yaizacalle@gmail.com');
-INSERT INTO persona VALUES ('', 'Carla', 'Escribano','91' ,'42821952', '1192381298','Av. Varela 8927', 'Carlaescriban@gmail.com');
-INSERT INTO persona VALUES ('', 'Jana', 'Sala', '72', '23819214', '1139281298','Matanza 8275', 'Janasalon@gmail.com');
-INSERT INTO persona VALUES ('', 'Xavi', 'Prada','62' ,'42019182', '1109128932','Av. Cobo 5125', 'Xavipradera@gmail.com');
-INSERT INTO persona VALUES ('', 'Sara', 'Corral', '34', '29102728', '1112983298','Av Caseros 5254', 'Saracorral@gmail.com');
-INSERT INTO persona VALUES ('', 'Claudia', 'Sanjuan','32' ,'38918274', '1123874278','Av. Cordoba 5282', 'Claudiasanju@gmail.com');
-
-
+INSERT INTO persona VALUES ('', 'Bilal', 'Novo', '65', '34871890', '+54 11 3498-1289','Av Asamblea 5125', 'Bilalnovo@gmail.com');
+INSERT INTO persona VALUES ('', 'Eleuterio', 'Aliaga','43' ,'23890197', '+54 11 8734-2187','Tejedor 252', 'Eleutorioaliaga@gmail.com');
+INSERT INTO persona VALUES ('', 'Younes', 'Vazquez', '87', '32892987', '+54 11 1234-6754','Avellaneda 8924', 'Younesvazquez@gmail.com');
+INSERT INTO persona VALUES ('', 'Ona', 'Hinojosa','37' ,'39872910', '+54 11 2189-2389','Av Pavon 8294', 'Onahinojosa@gmail.com');
+INSERT INTO persona VALUES ('', 'Yaiza', 'Calle', '74', '28901827', '+54 11 9128-2398','Av Jose Maria Moreno 5829', 'Yaizacalle@gmail.com');
+INSERT INTO persona VALUES ('', 'Carla', 'Escribano','91' ,'42821952', '+54 11 9238-1298','Av. Varela 8927', 'Carlaescriban@gmail.com');
+INSERT INTO persona VALUES ('', 'Jana', 'Sala', '72', '23819214', '+54 11 3928-1298','Matanza 8275', 'Janasalon@gmail.com');
+INSERT INTO persona VALUES ('', 'Xavi', 'Prada','62' ,'42019182', '+54 11 0912-8932','Av. Cobo 5125', 'Xavipradera@gmail.com');
+INSERT INTO persona VALUES ('', 'Sara', 'Corral', '34', '29102728', '+54 11 1298-3298','Av Caseros 5254', 'Saracorral@gmail.com');
+INSERT INTO persona VALUES ('', 'Claudia', 'Sanjuan','32' ,'38918274', '+54 11 2387-4278','Av. Cordoba 5282', 'Claudiasanju@gmail.com');
 
 
 
@@ -98,13 +96,30 @@ INSERT INTO persona VALUES ('', 'Claudia', 'Sanjuan','32' ,'38918274', '11238742
 
 
 
-INSERT INTO persona VALUES ('', 'Ana', 'Blanco','32' ,'38918375', '1126474278','Bolivia 546', 'Blancoana@gmail.com');
-INSERT INTO persona VALUES ('', 'Valentina', 'Mari','50' ,'30745274', '1123869978','Terrada 1259', 'Valenmari33@gmail.com');
-INSERT INTO persona VALUES ('', 'Viviana', 'Espejo','22' ,'40421276', '1123875888','Jonte 2008', 'Viviespejo04@gmail.com');
-INSERT INTO persona VALUES ('', 'Juan', 'Carro','52' ,'28976674', '1123987778','Campana 987', 'Juanchicarro@gmail.com');
-INSERT INTO persona VALUES ('', 'Thiago', 'Gabarri','37' ,'40916554', '1123879776','Jonte 2819', 'Thiagogaba@gmail.com');
-INSERT INTO persona VALUES ('', 'Marta', 'Lorenzo','46' ,'35988574', '1123876958','Bolivar 987', 'Martalorenzo82@gmail.com');
-INSERT INTO persona VALUES ('', 'Carmen', 'Liu','28' ,'43543274', '1124494278','9 de Julio 1202', 'Carmenliu3@gmail.com');
-INSERT INTO persona VALUES ('', 'Jacinto', 'Jimeno','52' ,'39968274', '1129964228','Lavallol 5844', 'jacijimeno@gmail.com');
-INSERT INTO persona VALUES ('', 'Mario', 'Ferrando','32' ,'38498274', '1123845226','Arregui 2122', 'MarioFerrando@gmail.com');
-INSERT INTO persona VALUES ('', 'Elena', 'Plasencia','37' ,'38867274', '1123872448','Sanabria 3668', 'Claudiasanju@gmail.com');
+
+
+INSERT INTO persona VALUES ('', 'Ana', 'Blanco','32' ,'38918375', '+54 11 2647-4278','Bolivia 546', 'Blancoana@gmail.com');
+INSERT INTO persona VALUES ('', 'Valentina', 'Mari','50' ,'30745274', '+54 11 2386-9978','Terrada 1259', 'Valenmari33@gmail.com');
+INSERT INTO persona VALUES ('', 'Viviana', 'Espejo','22' ,'40421276', '+54 11 2387-5888','Jonte 2008', 'Viviespejo04@gmail.com');
+INSERT INTO persona VALUES ('', 'Juan', 'Carro','52' ,'28976674', '+54 11 2398-7778','Campana 987', 'Juanchicarro@gmail.com');
+INSERT INTO persona VALUES ('', 'Thiago', 'Gabarri','36' ,'37916554', '+54 11 2387-9776','Jonte 2819', 'Thiagogaba@gmail.com');
+INSERT INTO persona VALUES ('', 'Marta', 'Lorenzo','46' ,'35988574', '+54 11 2387-6958','Bolivar 987', 'Martalorenzo82@gmail.com');
+INSERT INTO persona VALUES ('', 'Carmen', 'Liu','28' ,'43543274', '+54 11 2449-4278','9 de Julio 1202', 'Carmenliu3@gmail.com');
+INSERT INTO persona VALUES ('', 'Jacinto', 'Jimeno','52' ,'39968274', '+54 11 2996-4228','Lavallol 5844', 'jacijimeno@gmail.com');
+INSERT INTO persona VALUES ('', 'Mario', 'Ferrando','32' ,'38498274', '+54 11 2384-5226','Arregui 2122', 'MarioFerrando@gmail.com');
+INSERT INTO persona VALUES ('', 'Martina', 'Plasencia','36' ,'38344565', '+54 11 2112-3346','Sanabria 3328', 'Martuplase@gmail.com');
+INSERT INTO persona VALUES ('', 'Marco', 'Rojo','29' ,'38488562', '+54 11 5447-6548','Concordia 856', 'Marquitosrojo@gmail.com');
+INSERT INTO persona VALUES ('', 'Marcos', 'Navaja','58' ,'23867243', '+54 11 2387-6338','Monte Castro 3261', 'Navajitas233@gmail.com');
+INSERT INTO persona VALUES ('', 'Francisco', 'Gonzales','71' ,'20845786', '+54 11 2622-9558','Empedrado 2316', 'Piedrishtuba@gmail.com');
+INSERT INTO persona VALUES ('', 'Xoan', 'Pais','21' ,'41884223', '+54 11 2121-2998','Av. Lope de Verga 4522', 'xxJowigamer2xx@gmail.com');
+INSERT INTO persona VALUES ('', 'Ariel', 'Kupa','27' ,'38867274', '+54 11 2144-8445','Terrada 132', 'kupaarielgmail@gmail.com');
+INSERT INTO persona VALUES ('', 'Joaquin', 'Perez','41' ,'38713556', '+54 11 2332-7554','Peron 1974', 'Joacoperez1@gmail.com');
+INSERT INTO persona VALUES ('', 'Martin', 'Yubran','27' ,'38865211', '+54 11 2387-2448','Sanabria 2138', 'Yubranetamartin@gmail.com');
+INSERT INTO persona VALUES ('', 'Gloria', 'Feroldi','41' ,'32152235', '+54 11 2112-3556','Caseros 753', 'Gloriaferoldi@gmail.com');
+INSERT INTO persona VALUES ('', 'Mio', 'Akiyama','30' ,'37699851', '+54 11 2214-9947','Francisco Borges 2214', 'Mioakiyama1@gmail.com');
+INSERT INTO persona VALUES ('', 'Ritsu', 'Tainaka','30' ,'37662154', '+54 11 4584-3325','Francisco Borges 2214', 'Ritsuonthedrums@gmail.com');
+INSERT INTO persona VALUES ('', 'Mugi', 'Kotobuki','30' ,'37148455', '+54 11 4852-3362','Francisco Borges 2214', 'Mugikotobuki@gmail.com');
+INSERT INTO persona VALUES ('', 'Yui', 'Hirasawa','30' ,'37451268', '+54 11 6221-1544','Francisco Borges 2214', 'Yuihirasawa1@gmail.com');
+INSERT INTO persona VALUES ('', 'Azusa', 'Nakano','29' ,'38621147', '+54 11 1552-3622','Francisco Borges 2214', 'Azusanakano@gmail.com');
+INSERT INTO persona VALUES ('', 'Rita', 'Estefan','49' ,'21254486', '+54 11 4211-1555','Edison 1151', 'ritaestefan@gmail.com');
+INSERT INTO persona VALUES ('', 'Nahele', 'Moya','32' ,'34226531', '+54 11 6422-6612','Ayacucho 212', 'nahelecordobe@gmail.com');
