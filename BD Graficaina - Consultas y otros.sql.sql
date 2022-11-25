a3e466bdea2f9aef570fa5292832b45867ffa553
winterishtuba <winterishtuba00@gmail.com>
2022-11-25 01:22:38 -0300

    consultas_1

--- BD Graficaina - Consultas y otros.sql.sql
+++ BD Graficaina - Consultas y otros.sql.sql
@@ -11,3 +11,12 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+SELECT estado, fecha_pedido FROM pedido WHERE estado LIKE  'cancelado' AND DATE BETWEEN '2015-9-20' AND '2015-10-25';
+SELECT COUNT(Estado) AS cantidad FROM pedido Where Estado LIKE 'entregado' ;
+SELECT COUNT(id_empleado), puesto From empleado GROUP BY puesto;
+SELECT Direccion, ciudad FROM sucursal ORDER BY ciudad DESC;
+SELECT  Nombre, apellido FROM persona ORDER BY Id_persona ASC;	
+SELECT  COUNT(producto), tipo_producto FROM Producto GROUP BY tipo_producto;
+SELECT estado, fecha_entrega FROM pedido WHERE estado LIKE 'pendiente' AND fecha_entrega >= '2021-06-20';
+
