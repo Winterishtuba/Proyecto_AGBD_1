b454c0fc700f384658e0874edbd84d40c1cc8d63
Xtres5 <Federicoz2005@gmail.com>
2022-11-27 19:57:05 -0300

    ghia no labura, quiere todo de arriba

--- BD Graficaina - Consultas y otros.sql.sql
+++ BD Graficaina - Consultas y otros.sql.sql
@@ -12,7 +12,25 @@ END$$
 
 DELIMITER ;
 
+DROP TRIGGER IF EXISTS identpago_bi;
+   DELIMITER //
+    CREATE TRIGGER identpago_bi BEFORE INSERT
+    ON pago FOR EACH ROW 
+    BEGIN
+    DECLARE id_persona1 INT;
+    SELECT id_persona INTO id_persona1 FROM pedido WHERE id_pedido = NEW.id_pedido;
+    IF id_persona1 != NEW.id_persona THEN
+    SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = "el pago no corresponde a la persona.";
+     END IF; 
+   END //
+DELIMITER ;
+
+
+
+
 CREATE VIEW Empleados AS SELECT id_empleado, puesto FROM empleado ORDER BY puesto;
+
+
 
 SELECT estado, fecha_pedido FROM pedido WHERE estado LIKE  'cancelado' AND fecha_pedido BETWEEN '2015-9-20' AND '2015-10-25';
 SELECT COUNT(Estado) AS cantidad FROM pedido Where Estado LIKE 'entregado' ;
@@ -23,3 +41,4 @@ SELECT COUNT(id_producto), gama FROM Producto GROUP BY gama;
 SELECT estado, fecha_entrega FROM pedido WHERE estado LIKE 'pendiente' AND fecha_entrega >= '2021-06-20';
 SELECT * FROM Empleado WHERE puesto IN (SELECT puesto FROM empleado Where sueldo > 5000);
 SELECT Persona.ID_persona, pedido.ID_Pedido FROM persona INNER JOIN pedido ON Persona.ID_persona = pedido.ID_Pedido;
+
